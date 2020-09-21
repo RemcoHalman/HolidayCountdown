f6df21c3ecf54483c59dfe1135660e8d46a509ea
RemcoHalman <remco.halman@gmail.com>
2020-09-21 13:08:48 +0200

    Added Comments

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <!-- For loop to iterate each item in the json file -->
         <div v-for="day in days" :key="day">
             <Holidays v-bind:holiday="day" />
         </div>
@@ -7,12 +8,15 @@
 </template>
 
 <script>
+// Import Json file
 import holidays_json from "./assets/json/holidays.json";
+// Import Components
 import Holidays from "./components/Holidays.vue";
 
 export default {
     name: "App",
     data() {
+        // Return the json file as a object to loop (line: 3)
         return { days: holidays_json };
     },
     components: {
--- src/components/Holidays.vue
+++ src/components/Holidays.vue
@@ -26,31 +26,42 @@
 export default {
     name: "Holidays",
     props: {
+        // Get passed prop and setting it to a variable
         holiday: JSON,
     },
     data() {
         return {
+            // Initialising time as a empty string
             time: "",
         };
     },
     computed: {},
     methods: {
         refactor_time: function(time) {
+            // Calculate the days
             var days = Math.floor(time / (1000 * 60 * 60 * 24));
+            // Calculate the hours in above day
             var hours = Math.floor(
                 (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
             );
+            // Calculate the minutes in above hour
             var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+            // Calculate the seconds in above minute
             var seconds = Math.floor((time % (1000 * 60)) / 1000);
+            // Return data to the template
             return days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
         },
         ticker: function(year, month, day) {
             var data_text = year + "-" + month + "-" + day;
+            // Get the json date entered
             var date = new Date(data_text).getTime();
+            // Get the current time and date
             var now = new Date().getTime();
+            // Calculate the difference
             var time = date - now;
+            // Running the format function
             var days_until = this.refactor_time(time);
-            // console.log(days_until);
+            // Returns the formated time until
             return days_until;
         },
     },
